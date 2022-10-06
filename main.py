ef2d7ce92ca2a68333c0a169b27d6fe2d702f9b4
Eduardo <edugc20142hotmail.com>
2022-10-06 18:01:33 -0300

    Atualizações

---
+++ main.py
@@ -0,0 +1,13 @@
+import qrcode
+
+
+# data = 'Não esqueca de se inscrever'
+
+# img = qrcode.make(data)
+
+# qr = qrcode.QRCode(version=1, box_size=10, border=5)
+# qr.add_data(data)
+# qr.make(fit=True)
+# img = qr.make_image(fill_color='red', back_color='white')
+# img.save('C:/Users/Eduardo.santos/Documents/Programação/Python Projetos/GeradorDeQRCode/MeuQRCodeNew.png')
+
